61fb06218c6eeaa4d77874b38ee12b661f7fdc5a
Gyubong Lee <jopemachine@naver.com>
2021-12-05 21:02:00 +0900

    Update

--- cp.sh
+++ cp.sh
@@ -1,2 +1,3 @@
 #!/usr/local/bin/zsh
-cp ~/.vimrc ./vimrc && git reset --soft HEAD^1 && git add * && git commit -m "Update" && git push origin master --force
+CUR_DIR="$(dirname "$(realpath "$0")")"
+cd ${CUR_DIR} && cp ~/.vimrc ./.vimrc && git reset --soft HEAD^1 && git add . && git commit -m "Update" && git push origin master --force
