0978209215739588a97faabd4c8193246668e1b4
Gyubong Lee <jopemachine@naver.com>
2022-05-16 20:18:20 +0900

    🔨 Update configs

--- copy-config-and-push.sh
+++ copy-config-and-push.sh
@@ -4,31 +4,31 @@ CUR_DIR="$(dirname "$(realpath "$0")")"
 cd ${CUR_DIR}
 
 # neovim
-cp ~/.config/nvim/init.vim ./init.vim
+cp ~/.config/nvim/init.vim ./neovim/init.vim
 
 # lunarvim
-cp ~/.config/lvim/config.lua ./config.lua
+cp ~/.config/lvim/config.lua ./lunarvim/config.lua
 
 # zsh
-cp ~/.zshrc ./.zshrc
+cp ~/.zshrc ./zsh/zshrc
 
 # starship
-cp ~/.config/starship.toml ./.config/starship.toml
+cp ~/.config/starship.toml ./starship/starship.toml
 
 # tmux
-cp ~/.tmux.conf ./.tmux.conf
+cp ~/.tmux.conf ./tmux/.tmux.conf
 
 # git
-cp ~/.gitconfig ./.gitconfig
+cp ~/.gitconfig ./git/.gitconfig
 
 # chrome extensions
 if test -f "~/Downloads/extensions.html";
 then
-	cp ~/Downloads/extensions.html ./chrome-extensions.html
+	cp ~/Downloads/extensions.html ./chrome/chrome-extensions.html
 fi
 
 # vscode extensions
-code --list-extensions | xargs -L 1 > vscode-extensions
+code --list-extensions | xargs -L 1 > ./vscode/vscode-extensions
 
 git add . && git commit -m "🔨 Update configs" && git push origin master --force
 
