1f22755bed88236f4f3a1bbf6e7dd52b9e37cb99
Gyubong Lee <jopemachine@naver.com>
2022-06-05 14:56:01 +0900

    🔨 Update configs

--- lunarvim/config.lua
+++ lunarvim/config.lua
@@ -22,7 +22,7 @@ lvim.format_on_save = false
 -- *
 lvim.leader = "space"
 
--- Use Tab instead of space
+-- Use Tab instead of space at default
 vim.cmd([[
 	set autoindent
 	set noexpandtab
@@ -140,18 +140,20 @@ lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Project
 -- Other utilities key settings
 lvim.builtin.which_key.mappings["u"] = {
 	name = "Utility",
-	u = { ":UndotreeToggle<cr>:UndotreeFocus<cr>", "UndotreeToggle" },
-	s = { ":set spell!<cr>", "Toggle spell" },
-	g = { ":Glow<cr>", "Glow" },
-	b = { ":GitBlameToggle", "GitBlame Toggle" },
-	t = { ":Telescope vim_options<cr>", "View vim options" },
-	r = { ":Telescope grep_string<cr>", "Grep strings" },
+	u = { ":UndotreeToggle<cr>:UndotreeFocus<cr>", "Toggle Undotree" },
+	s = { ":set spell!<cr>", "Toggle Spell Check" },
+	g = { ":Glow<cr>", "Toggle Glow Window" },
+	b = { ":GitBlameToggle<cr>", "Toggle GitBlame Plugin" },
+	o = { ":Telescope vim_options<cr>", "View Vim Options" },
+	r = { ":Telescope grep_string<cr>", "Grep Strings" },
+	e = { ":set expandtab!<cr>", "Toggle Expandtab" },
+	a = { ":Ranger<cr>", "Ranger" },
 }
 
 lvim.builtin.which_key.mappings["C"] = {
 	name = "Code manipulate",
-	s = { ":SplitjoinSplit<cr>", "Switch function to multiline" },
-	j = { ":SplitjoinJoin<cr>", "Switch function to singleline" },
+	s = { ":SplitjoinSplit<cr>", "Switch Function to Multiline" },
+	j = { ":SplitjoinJoin<cr>", "Switch Function to Singleline" },
 }
 
 -- *
@@ -358,7 +360,12 @@ lvim.plugins = {
 
   -- Git
   { "kdheepak/lazygit.nvim" },
-  { "f-person/git-blame.nvim" },
+  {
+    "f-person/git-blame.nvim",
+    config = function ()
+        vim.cmd("let g:gitblame_enabled = 0")
+    end
+  },
 
   -- Github
   {
@@ -369,42 +376,6 @@ lvim.plugins = {
   {
     "folke/trouble.nvim",
     requires = "kyazdani42/nvim-web-devicons",
-    config = function()
-      require("trouble").setup({
-        position = "bottom", -- position of the list can be: bottom, top, left, right
-        height = 10, -- height of the trouble list when position is top or bottom
-        width = 50, -- width of the list when position is left or right
-        icons = true, -- use devicons for filenames
-        mode = "document_diagnostics",
-        action_keys = { -- key mappings for actions in the trouble list
-          -- map to {} to remove a mapping, for example:
-          -- close = {},
-          close = "q", -- close the list
-          cancel = "<esc>", -- cancel the preview and get back to your last window / buffer / cursor
-          refresh = "r", -- manually refresh
-          jump = { "<cr>", "<tab>" }, -- jump to the diagnostic or open / close folds
-          open_split = { "<c-x>" }, -- open buffer in new split
-          open_vsplit = { "<c-v>" }, -- open buffer in new vsplit
-          open_tab = { "<c-t>" }, -- open buffer in new tab
-          jump_close = { "o" }, -- jump to the diagnostic and close the list
-          toggle_mode = "m", -- toggle between "workspace" and "document" diagnostics mode
-          toggle_preview = "P", -- toggle auto_preview
-          hover = "K", -- opens a small popup with the full multiline message
-          preview = "p", -- preview the diagnostic location
-          close_folds = { "zM", "zm" }, -- close all folds
-          open_folds = { "zR", "zr" }, -- open all folds
-          toggle_fold = { "zA", "za" }, -- toggle fold of current file
-          previous = "k", -- preview item
-          next = "j", -- next item
-        },
-        indent_lines = true, -- add an indent guide below the fold icons
-        auto_open = false, -- automatically open the list when you have diagnostics
-        auto_close = true, -- automatically close the list when you have no diagnostics
-        auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
-        auto_fold = false, -- automatically fold a file trouble list at creation
-        use_diagnostic_signs = true, -- enabling this will use the signs defined in your lsp client
-      })
-    end,
   },
 }
 
